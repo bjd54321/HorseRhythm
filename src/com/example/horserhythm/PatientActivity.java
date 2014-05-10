c89bf570b0ddd59b7a637d56890168b6412ea6f9
Brian <bjd54321@gmail.com>
2014-05-10 13:50:15 +0300

    After understanding how GitHub works

--- src/com/example/horserhythm/PatientActivity.java
+++ src/com/example/horserhythm/PatientActivity.java
@@ -54,10 +54,7 @@ public class PatientActivity extends Activity {
     
     private Handler mHandler;
     
-    // Adding a comment to test GitHub
-    // Adding comment number 2
-    // And a third comment
-    // Forth comment
+    // Now I understand how to use GitHub
     
     // have in mind that: http://stackoverflow.com/questions/11407943/this-handler-class-should-be-static-or-leaks-might-occur-incominghandler
     // in this case we should be fine as no delayed messages are queued
